54e94d63fbbb022f76e9011ad6d11c5274406c9f
Rory <najar1412@users.noreply.github.com>
2022-11-22 15:04:16 -0500

    changed placeables to show all instances

--- src/ui/PlacementDisplay.jsx
+++ src/ui/PlacementDisplay.jsx
@@ -6,21 +6,24 @@ const PlacementDisplay = (props) => {
     props.setSelectableToPlace(e.currentTarget.getAttribute("data-item-name"));
   }
 
-  const listItems = props.placeables.map((item, index) => (
-    <div
-      onClick={handleClick}
-      key={index}
-      data-item-name={item.name}
-      className="button button-secondary button-fit justify-between py-2 px-8 mb-4"
-    >
-      <div className="w-[150px]">{item.name}</div>
-      <div>x{item.quantity}</div>
-    </div>
-  ));
+  const listItems = props.placeables.map((item, index) => {
+    return (
+      <div
+        onClick={handleClick}
+        key={index}
+        data-item-name={item.name}
+        className="button button-secondary button-fit justify-between py-2 px-8 mb-4"
+      >
+        <div className="w-[150px]">{item.name}</div>
+      </div>
+    );
+  });
 
   return (
     <div id="action-menu" className="pointer-events-auto w-fit">
-      {listItems.length ? <p className="text-xs font-bold">Placeable</p> : null}
+      {listItems.length ? (
+        <p className="text-xs font-bold">Placeables</p>
+      ) : null}
       {listItems}
     </div>
   );
