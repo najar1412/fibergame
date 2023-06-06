e459f7d6c12c0637b1bc0342b6d58321b78ee321
= <=>
2023-06-06 16:27:27 +0100

    refactor button

--- src/engine/ui/components/TimedButton.jsx
+++ src/engine/ui/components/TimedButton.jsx
@@ -1,17 +1,17 @@
 const TimedButton = (props) => {
   function handleAction(e) {
-    console.log(props);
     e.target.classList.add("button-disabled");
     props.startTaskTimer(props.taskData.duration, props.handleTask);
     props.setCharacterTask(props.taskData);
   }
 
-  return props.disabled ? (
-    <div className="button button-fit button-disabled mb-4">
-      {props.characterTask ? props.characterTask.verb : props.label}
-    </div>
-  ) : (
-    <div onClick={handleAction} className="button button-fit mb-4">
+  return (
+    <div
+      onClick={handleAction}
+      className={`button button-fit mb-4 ${
+        props.disabled && "button-disabled"
+      }`}
+    >
       {props.characterTask ? props.characterTask.verb : props.label}
     </div>
   );
